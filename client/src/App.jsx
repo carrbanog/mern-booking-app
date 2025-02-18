685dad6e88226685bf6917f9a59fd6f69febf899
김민기 <loopang5954@gmail.com>
2025-02-18 12:42:07 +0900

    header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { FaAirbnb, FaSearch } from "react-icons/fa";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+    <div>
+      <header className="flex justify-between p-4">
+        <a href="" className="flex items-center gap-1">
+          <FaAirbnb className="w-8 h-8" />
+          <span className="font-bold text-xl">airbnb</span>
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+        <div className="flex gap-2 border border-gray-300 rounded-full py-2 px-4 shadow-md shadow-gray-300">
+          <div>Anywhere</div>
+          <div className="border-l border-gray-300 "></div>
+          <div>Any week</div>
+          <div className="border-l border-gray-300 "></div>
+          <div className="">Add guests</div>
+          <button className="bg-[#F5385D] rounded-full text-white p-1">
+            <FaSearch className="w-4, h-4" />
+          </button>
+        </div>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
