7d16439ce29fc877f4e02a708b3fd82064609f19
김민기 <loopang5954@gmail.com>
2025-02-20 10:10:08 +0900

    login, register

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,20 @@
 import "./App.css";
-import { FaAirbnb, FaSearch } from "react-icons/fa";
+import { Route, Routes } from "react-router-dom";
+import IndexPage from "./components/IndexPage";
+import LoginPage from "./components/LoginPage";
+import Layout from "./Layout";
+import RegisterPage from "./components/RegisterPage";
 
 function App() {
   return (
-    <div>
-      <header className="flex justify-between p-4">
-        <a href="" className="flex items-center gap-1">
-          <FaAirbnb className="w-8 h-8" />
-          <span className="font-bold text-xl">airbnb</span>
-        </a>
-        <div className="flex gap-2 border border-gray-300 rounded-full py-2 px-4 shadow-md shadow-gray-300">
-          <div>Anywhere</div>
-          <div className="border-l border-gray-300 "></div>
-          <div>Any week</div>
-          <div className="border-l border-gray-300 "></div>
-          <div className="">Add guests</div>
-          <button className="bg-[#F5385D] rounded-full text-white p-1">
-            <FaSearch className="w-4, h-4" />
-          </button>
-        </div>
-      </header>
-    </div>
+    <Routes>
+      {/* <Route path="/" element={<IndexPage />} /> */}
+      <Route path="/" element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Route>
+    </Routes>
   );
 }
 
